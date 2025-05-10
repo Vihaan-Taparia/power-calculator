979ab3a36452333c56375079d5239a78374c3b61
Vihaan-Taparia <mayataparia@gmail.com>
2025-05-10 22:14:00 +0530

    Add files via upload

---
+++ power calculator.py
@@ -0,0 +1,7 @@
+power=int(input("Enter a character....."))
+powerer=int(input("Enetr the number you want to put the above number to power at...."))
+
+ans=power**powerer
+
+print("The answer of",power,"to the power of",powerer,"is",ans)
+
